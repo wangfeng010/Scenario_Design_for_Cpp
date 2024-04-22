d39f4b932dee41bbdae22276c6b54acdcfa0e961
wangfeng010 <1977513290@qq.com>
2024-04-23 07:38:56 +0800

    Class Point

--- include/planning_base.h
+++ include/planning_base.h
@@ -14,3 +14,26 @@ constexpr auto PI = 3.14159265358979323846;//圆周率
 constexpr auto SHOWCIRCLE = false;//是否绘制轨迹
 constexpr auto DELAYTIME = 20;//间隔时间，ms
 constexpr auto CHANGETIME = 1000;//换档时间，ms
+
+/******************************************点******************************************/
+class Point//点
+{
+public:
+	Point() = default;
+	Point(const double& p_x, const double& p_y, const double& p_theta = 0.0, const double& p_R = 0.0);
+	void showPoint();//绘制点
+	void pointMove(const double& speed_x, const double& speed_y);//点的移动
+	void pointTurn(const Point& center, const double& turn_speed);//点绕center旋转，turn_speed为角速度
+	double distanceTo(const Point& p) const;//距离
+	double thetaTo(const Point& p) const;//this->相对于p角度
+
+public:
+	double x;
+	double y;
+	double thetaP = 0.0;//角度
+	double Rp = 0.0;//旋转半径
+	int r = 5;//绘制半径
+};
+
+/******************************************全局函数******************************************/
+void delay(const int& time); //延时函数，单位ms
--- src/planning_base.cpp
+++ src/planning_base.cpp
@@ -1,1 +1,41 @@
 #include "planning_base.h"
+
+/******************************************点******************************************/
+Point::Point(const double& p_x, const double& p_y, const double& p_theta, const double& p_R) : x(p_x), y(p_y), thetaP(p_theta), Rp(p_R)//点
+{
+
+}
+
+void Point::showPoint()//绘制点
+{
+	setfillcolor(BLACK);
+	solidcircle(x, y, r);
+}
+
+void Point::pointMove(const double& speed_x, const double& speed_y)//点的移动
+{
+	x += speed_x;
+	y += speed_y;
+}
+
+void Point::pointTurn(const Point& center, const double& turn_speed)//点的旋转（p绕center旋转）
+{
+	thetaP += turn_speed;//turn_speed>0，为绕着center逆时针行驶，<0则为绕着center顺时针行驶
+	x = Rp * cos(thetaP) + center.x;
+	y = -Rp * sin(thetaP) + center.y;//考虑到eaxyX坐标系，加负号
+}
+
+double Point::distanceTo(const Point& p) const//距离
+{
+	return hypot(x - p.x, y - p.y);
+}
+
+/******************************************全局函数******************************************/
+void delay(const int& time) //延时函数，单位ms
+{
+	clock_t  now = clock();
+	while (clock() - now < time)
+	{
+
+	}
+}
